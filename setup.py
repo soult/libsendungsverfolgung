86c5bc6135c65301187ab7bb77e6c49a7f9781e1
David Triendl <david@triendl.name>
2014-11-21 00:42:03 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(
+    name="libsendungsverfolgung",
+    version="0.1.0a1",
+    description="Python3 library for tracking parcels shipped with various couriers",
+    long_description="A Python3 library for tracking parcels and other shipments. It abstracts the various status messages from each shipping company into a simple hierachy of delivery events.",
+    author="David Triendl",
+    author_email="david@triendl.name",
+    packages=["libsendungsverfolgung"],
+    install_requires=["requests>=2.0.0"],
+)
